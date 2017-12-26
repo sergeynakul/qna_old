41195329db39a9ebfc295924525263488acf2dc4
Sergey <nakul.sv@gmail.com>
2017-12-26 17:26:43 +0200

    User can create answer 2

--- spec/acceptance/create_answers_spec.rb
+++ spec/acceptance/create_answers_spec.rb
@@ -9,7 +9,7 @@ feature 'User answer', %q{
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
 
-  scenario 'authenticated user create answer' do
+  scenario 'authenticated user create answer', js: true do
     sign_in(user)
     visit question_path(question)
 
