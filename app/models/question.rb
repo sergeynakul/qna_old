8432519acecec22df4f2c29a5bc25b76385c95dd
Sergey <nakul.sv@gmail.com>
2017-12-29 18:51:25 +0200

    User can add files to answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
 	validates :title, :body, presence: true
 	has_many :answers
-	has_many :attachments
+	has_many :attachments, as: :attachmentable
 
 	accepts_nested_attributes_for :attachments
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
+  it { should have_many :attachments }
   it { should validate_presence_of :body }
+  it { should accept_nested_attributes_for :attachments }
 end
